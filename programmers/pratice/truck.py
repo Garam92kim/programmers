12b837716575bda13ec261640c5928410380e6e3
garam92kim <rkfka0306@naver.com>
2022-02-06 22:17:10 +0900

    22-02-06

--- programmers/pratice/truck.py
+++ programmers/pratice/truck.py
@@ -2,6 +2,7 @@ from collections import deque
 
 def solution(bridge_length, weight, truck_weights):
     truck_weights = deque(truck_weights) #pop을 편하게 하기 위해 deque 사용
+    print(truck_weights)
     bridge = deque([0 for _ in range(bridge_length)]) #다리 위 Truck 확인 위한 배열
     bridge_weight = 0 # 현재 다리 위 무게
     time = 0 # 통과 시간 확인
@@ -10,17 +11,18 @@ def solution(bridge_length, weight, truck_weights):
         out = bridge.popleft() # 다리위에서 나오면 pop 실행
         bridge_weight -= out # 다리위 무게 감소
         time += 1 # 다리위에 차 있으면 시간 계속 증가
-        if truck_weights: # truck_weights가 존재하면
-            if truck_weights[0] + bridge_weight <= weight:
+        if truck_weights: # truck_weight가 존재하면
+            if  bridge_weight + truck_weights[0] <= weight:
                 # pop했기 때문에 항상 0번째 값만 비교해도 됨
                 temp = truck_weights.popleft()
                 bridge_weight += temp
                 bridge.append(temp)
-                print (bridge)
+                #print ("여기", bridge)
             else:
                 bridge.append(0)
-                print (bridge)
+               # print(bridge)
+               # print ("else", bridge)
     print (time)
     return time
 
-solution (5, 30, [10,10,10,10,1,2,3,4,5,6,7])
+solution (2, 10, [7, 4, 5, 6])
