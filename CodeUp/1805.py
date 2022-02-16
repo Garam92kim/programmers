3eb28ab5e8dbff810165c4cc47b94dac00435846
garam92kim <rkfka0306@naver.com>
2022-02-17 08:36:00 +0900

    2022-02-17

---
+++ CodeUp/1805.py
@@ -0,0 +1,17 @@
+from collections import deque
+
+"""
+입체기동장치 1~100 식별번호, 가스보유량10000을 같이관리
+"""
+kit_ea = int(input())
+kit_num_gas = {}
+
+for i in range(kit_ea):
+    kit_number, gas = input().split()
+    kit_number = int(kit_number)
+    gas = int (gas)
+    kit_num_gas.append(kit_number, gas)
+    
+for i in range(0, kit_ea-1):
+    print(kit_ea, kit_num_gas[i][i])
+    
--- SideProject/AutomationForWCBS/AutomationforWCBS.py
+++ SideProject/AutomationForWCBS/AutomationforWCBS.py
@@ -1,35 +1,35 @@
-from genericpath import getsize
 import threading
-import os
-i = 1 # log file 번호
-count = 0 # Timer thread 동작을 위함
-log_file = open("d:\Python\programmers\pratice\AutomationforWCBS(%d).txt" %i, 'r') # Log 저장 위치
-log_file_size = os.path.getsize("d:\Python\programmers\pratice\AutomationforWCBS(%d).txt" %i)
+import serial
 
-def startTimer(): #Timer thread 동작을 위한 함수
+count = 0 # Timer thread 동작을 위함
+port = "COM3"
+baud = 115200
+ser = serial.Serial(port, baud)
+
+def time_thread(): #Timer thread 동작을 위한 함수
     global count
-    timer = threading.Timer(10, startTimer)
+    timer = threading.Timer(1, time_thread)
     timer.start()
     count += 1
     print(count)
-    if count > 12: #80초 경과 시 종료
+    if count > 130: #80초 경과 시 종료
         timer.cancel()
-        log_file.close()
-startTimer()
+        ser.close()
+        
+time_thread()
+
+if ser.isOpen() == False:
+    ser.open()
 
 while True:
-    try:
-        lines = log_file.readlines() # Log 문자열 저장
-        for i in range(0, len(lines)): # SW NUMBER 포함된 문자열 있으면 pass
-            if "SW_NUMBER" in lines[i]:
+    if ser.readable():
+        res = ser.readline()
+        try:
+            res_str = res.decode("UTF-8")
+            if "SW_NUMBER" in res_str:
                 count = 0 # WCBS Init 되는 정보가 오면 Count 초기화
-                print("PASS")
-            elif log_file_size > 2130:
-                log_file.close()
-                i += 1
-                log_file = open("d:\Python\programmers\pratice\AutomationforWCBS(%d).txt" %i, 'r') # Log 저장 위치
-                log_file_size = os.path.getsize("d:\Python\programmers\pratice\AutomationforWCBS(%d).txt" %i)
-                continue
-    except ValueError as e:
-        print(e)
-        break
+                print("PASS, Count Init")
+
+        except ValueError as e:
+            print(e)
+            continue
